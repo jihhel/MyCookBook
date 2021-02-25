f1b773aef0671f871ece6c48aa2f20231c8b8f7c
Jonathan Lecointe <jonathan@energic.io>
2021-02-25 16:22:16 +0100

    Updates

--- src/Utils/Patterns.ts
+++
@@ -1,5 +0,0 @@
-export type Pattern = string;
-
-export function parsePatterns(str: string): Pattern[] {
-    return str.split(" ").map(s => s.trim()).filter(s => s.length > 0);
-};
--- src/Utils/functions.ts
+++
@@ -1,7 +0,0 @@
-export function forall(list: any[], predicate: (a: any) => boolean): boolean {
-    return list.filter(predicate).length === list.length;
-}
-
-export function exists(list: any[], predicate: (a: any) => boolean): boolean {
-    return list.filter(predicate).length !== 0;
-}
